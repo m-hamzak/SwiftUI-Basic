7562bf2ce7b67bbb0ed8a55f5120c69ada35d5d0
m-hamzak <m_hamzak1997@yahoo.com>
2025-04-11 21:03:32 +0500

    model data implemented

---
+++ SwiftUI-Basic/Model/Experience.swift
@@ -0,0 +1,26 @@
+//
+//  Experience.swift
+//  SwiftUI-Basic
+//
+//  Created by Hamza Khalid on 11/04/2025.
+//
+import SwiftUI
+
+struct Experience: Hashable, Codable {
+    var id: Int
+    var name: String
+    var city: String
+    var state: String
+    var country: String
+    var roleType: String
+    var roleLocation: String
+    var startDate: String
+    var endDate: String
+    var designation: String
+    var description: String
+    
+    private var imageName: String
+        var image: Image {
+            Image(imageName)
+        }
+}
